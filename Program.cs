5af44a7339e9966a67f2e744e64e8929b281ac03
shofior <107652804+shofior@users.noreply.github.com>
2022-06-17 01:25:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Chocolates
+{
+    class Program
+    {
+        static int buy(int dollars)
+        {          
+            int chocolates =0;
+            int wrapes =0;
+            while (dollars > 0)
+            {
+                dollars--;
+                chocolates++;
+                wrapes++;
+                if (wrapes % 3 == 0) 
+                { 
+                    wrapes = 0;
+                    chocolates++;
+                    wrapes++;
+                }
+            }
+            Console.WriteLine("Оберток осталось:{0}",wrapes);
+            return chocolates;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество денег");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = buy(a);
+            Console.WriteLine("Количество купленых шоколадок:{0}",b);          
+            Console.ReadLine();
+        }
+    }
+}
